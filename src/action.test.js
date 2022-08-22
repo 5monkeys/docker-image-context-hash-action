20d487664e475bf6982f8a4e2e5c72ecdae05772
Petter Friberg <petter_friberg@hotmail.com>
2022-08-22 13:18:22 +0200

    Include filenames in `git ls-tree` output (#2)
    Removes the need of generating an additional hash for only filenames

--- src/action.test.js
+++ src/action.test.js
@@ -10,39 +10,23 @@ describe("run", () => {
     process.env["INPUT_BUILD_CONTEXT"] = "./test";
     process.env["INPUT_EXTRA_TREE_OBJECTS"] =
       "test/.dockerignore\ntest/Dockerfile";
-    process.env["INPUT_EXTRA_VALUES"] = "test/.dockerignore\ntest/Dockerfile";
     // Expected hash generated from command below
-    // {\
-    //   git ls-tree -r --format='%(objectname)' --full-tree HEAD \
-    //     test/a/b \
-    //     test/context/file \
-    //     test/filename \
-    //     test/.dockerignore \
-    //     test/Dockerfile\
-    //   ; \
-    //   printf "test/a/b\ntest/context/file\ntest/filename\ntest/.dockerignore\ntest/Dockerfile" | \
-    //     git hash-object --stdin\
-    //   ;\
-    // } | git hash-object --stdin
+    // git ls-tree -r --format='%(objectname) %(path)' --full-tree HEAD \
+    //   test/a/b \
+    //   test/context/file \
+    //   test/filename \
+    //   test/.dockerignore \
+    //   test/Dockerfile | \
+    // git hash-object --stdin
     await run();
     // See https://github.com/actions/toolkit/issues/777 regarding the newline..
     expect(process.stdout.write).toHaveBeenLastCalledWith(
-      `::set-output name=hash::6d09883618bd309806be67847737b1a5a4aa1f40\n`
+      `::set-output name=hash::6c867c528d4bf0844d5176f78f6abfee0adfff2b\n`
     );
     // Expect a second run to produce the same output
     await run();
     expect(process.stdout.write).toHaveBeenLastCalledWith(
-      `::set-output name=hash::6d09883618bd309806be67847737b1a5a4aa1f40\n`
-    );
-  }, 30000);
-
-  test("errors when extra tree objects doesn't match", async () => {
-    const { run } = require("./action");
-    process.env["INPUT_BUILD_CONTEXT"] = "./test";
-    process.env["INPUT_EXTRA_TREE_OBJECTS"] = "non_matching_stuff";
-    await run();
-    expect(process.stdout.write).toHaveBeenLastCalledWith(
-      `::error::Object paths did not match the intended amount of objects in the tree (3 != 4)\n`
+      `::set-output name=hash::6c867c528d4bf0844d5176f78f6abfee0adfff2b\n`
     );
   }, 30000);
 
