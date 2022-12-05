d0f64d5c6009477bc6241487ca6ccdad6ca764cf
Krzysztof Jagiello <me@kjagiello.com>
2022-12-05 13:33:54 +0100

    Bump @actions/core to 1.10.0
    GitHub has deprecated the save-output command[1] in favor of special
    environment files[2]. The support for these environment files have been
    added in version 1.10.0 of @actions/core. This changeset bumps
    @actions/core and adjusts the tests accordingly.
    
    [1] https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/
    [2] https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#environment-files

--- src/action.test.js
+++ src/action.test.js
@@ -1,15 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
+const uuid = require("uuid");
 const { test, beforeEach, describe } = require("@jest/globals");
 
-beforeEach(() => {
-  process.stdout.write = jest.fn();
-});
+const UUID = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
+const DELIMITER = `ghadelimiter_${UUID}`;
 
 describe("run", () => {
+  beforeAll(() => {
+    const dirPath = path.join(__dirname, `test`);
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+  });
+
+  beforeEach(() => {
+    process.env["GITHUB_OUTPUT"] = "";
+    process.stdout.write = jest.fn();
+
+    jest.spyOn(uuid, "v4").mockImplementation(() => UUID);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  afterAll(() => {
+    const dirPath = path.join(__dirname, `test`);
+    fs.rmdirSync(dirPath);
+  });
+
   test("can output hash", async () => {
     const { run } = require("./action");
     process.env["INPUT_BUILD_CONTEXT"] = "./test";
     process.env["INPUT_EXTRA_TREE_OBJECTS"] =
       "test/.dockerignore\ntest/Dockerfile";
+    createFileCommandFile("OUTPUT");
     // Expected hash generated from command below
     // git ls-tree -r --format='%(objectname) %(path)' --full-tree HEAD \
     //   test/a/b \
@@ -19,15 +46,18 @@ describe("run", () => {
     //   test/Dockerfile | \
     // git hash-object --stdin
     await run();
-    // See https://github.com/actions/toolkit/issues/777 regarding the newline..
-    expect(process.stdout.write).toHaveBeenLastCalledWith(
-      `::set-output name=hash::6c867c528d4bf0844d5176f78f6abfee0adfff2b\n`
+    verifyFileCommand(
+      "OUTPUT",
+      serializeOutput("hash", "6c867c528d4bf0844d5176f78f6abfee0adfff2b")
     );
+    createFileCommandFile("OUTPUT");
     // Expect a second run to produce the same output
     await run();
-    expect(process.stdout.write).toHaveBeenLastCalledWith(
-      `::set-output name=hash::6c867c528d4bf0844d5176f78f6abfee0adfff2b\n`
+    verifyFileCommand(
+      "OUTPUT",
+      serializeOutput("hash", "6c867c528d4bf0844d5176f78f6abfee0adfff2b")
     );
+    expect(1).toBe(1);
   }, 30000);
 
   test("errors when given an invalid build context", async () => {
@@ -41,3 +71,25 @@ describe("run", () => {
     );
   });
 });
+
+function serializeOutput(key, value) {
+  return `${key}<<${DELIMITER}${os.EOL}${value}${os.EOL}${DELIMITER}${os.EOL}`;
+}
+
+function createFileCommandFile(command) {
+  const filePath = path.join(__dirname, `test/${command}`);
+  process.env[`GITHUB_${command}`] = filePath;
+  fs.appendFileSync(filePath, "", {
+    encoding: "utf8",
+  });
+}
+
+function verifyFileCommand(command, expectedContents) {
+  const filePath = path.join(__dirname, `test/${command}`);
+  const contents = fs.readFileSync(filePath, "utf8");
+  try {
+    expect(contents).toEqual(expectedContents);
+  } finally {
+    fs.unlinkSync(filePath);
+  }
+}
