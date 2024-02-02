dbe000fcbcd787c39c80ae625fcb7ddf2042e567
Petter Friberg <petter_friberg@hotmail.com>
2024-02-02 08:45:34 +0100

    Upgrade Node.js to 20

--- src/action.test.js
+++ src/action.test.js
@@ -8,17 +8,19 @@ jest.mock("uuid");
 
 const UUID = "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d";
 const DELIMITER = `ghadelimiter_${UUID}`;
+const OUTPUT_DIR = path.join(__dirname, `test`);
+const GITHUB_OUTPUT = path.join(OUTPUT_DIR, `OUTPUT`);
 
 describe("run", () => {
   beforeAll(() => {
-    const dirPath = path.join(__dirname, `test`);
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
+      fs.appendFileSync(GITHUB_OUTPUT, "", { encoding: `utf8` });
     }
   });
 
   beforeEach(() => {
-    process.env["GITHUB_OUTPUT"] = "";
+    process.env["GITHUB_OUTPUT"] = GITHUB_OUTPUT;
     process.stdout.write = jest.fn();
 
     jest.spyOn(uuid, "v4").mockImplementation(() => UUID);
@@ -26,11 +28,11 @@ describe("run", () => {
 
   afterEach(() => {
     jest.restoreAllMocks();
+    fs.truncateSync(GITHUB_OUTPUT);
   });
 
   afterAll(() => {
-    const dirPath = path.join(__dirname, `test`);
-    fs.rmdirSync(dirPath);
+    fs.rmSync(path.join(__dirname, `test`), { recursive: true });
   });
 
   test("can output hash", async () => {
@@ -38,7 +40,6 @@ describe("run", () => {
     process.env["INPUT_BUILD_CONTEXT"] = "./test";
     process.env["INPUT_EXTRA_TREE_OBJECTS"] =
       "test/.dockerignore\ntest/Dockerfile";
-    createFileCommandFile("OUTPUT");
     // Expected hash generated from command below
     // git ls-tree -r --format='%(objectname) %(path)' --full-tree HEAD \
     //   test/a/b \
@@ -47,19 +48,18 @@ describe("run", () => {
     //   test/.dockerignore \
     //   test/Dockerfile | \
     // git hash-object --stdin
-    await run();
-    verifyFileCommand(
-      "OUTPUT",
-      serializeOutput("hash", "6c867c528d4bf0844d5176f78f6abfee0adfff2b")
+    const expectedOutput = serializeOutput(
+      "hash",
+      "6c867c528d4bf0844d5176f78f6abfee0adfff2b"
     );
-    createFileCommandFile("OUTPUT");
+
+    await run();
+    expect(fs.readFileSync(GITHUB_OUTPUT, "utf8")).toEqual(expectedOutput);
+
     // Expect a second run to produce the same output
+    fs.truncateSync(GITHUB_OUTPUT);
     await run();
-    verifyFileCommand(
-      "OUTPUT",
-      serializeOutput("hash", "6c867c528d4bf0844d5176f78f6abfee0adfff2b")
-    );
-    expect(1).toBe(1);
+    expect(fs.readFileSync(GITHUB_OUTPUT, "utf8")).toEqual(expectedOutput);
   }, 30000);
 
   test("errors when given an invalid build context", async () => {
@@ -68,7 +68,7 @@ describe("run", () => {
     await run();
     expect(process.stdout.write).toHaveBeenLastCalledWith(
       expect.stringMatching(
-        `::error::The process '.*/docker' failed with exit code 1`
+        `::error::The process '.*/docker' failed with exit code 1[^0-9]`
       )
     );
   });
@@ -76,22 +76,4 @@ describe("run", () => {
 
 function serializeOutput(key, value) {
   return `${key}<<${DELIMITER}${os.EOL}${value}${os.EOL}${DELIMITER}${os.EOL}`;
-}
-
-function createFileCommandFile(command) {
-  const filePath = path.join(__dirname, `test/${command}`);
-  process.env[`GITHUB_${command}`] = filePath;
-  fs.appendFileSync(filePath, "", {
-    encoding: "utf8",
-  });
-}
-
-function verifyFileCommand(command, expectedContents) {
-  const filePath = path.join(__dirname, `test/${command}`);
-  const contents = fs.readFileSync(filePath, "utf8");
-  try {
-    expect(contents).toEqual(expectedContents);
-  } finally {
-    fs.unlinkSync(filePath);
-  }
 }
